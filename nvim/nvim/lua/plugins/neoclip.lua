0914c1e2d489c97f7bf8fcbb1d1c38f70a003fdb
Raphaël Bourgeas <rbourgeas@embl.fr>
2025-04-18 16:43:47 +0200

    Fixed require to return

--- nvim/nvim/lua/plugins/neoclip.lua
+++ nvim/nvim/lua/plugins/neoclip.lua
@@ -1,10 +1,10 @@
-require({
-    "AckslD/nvim-neoclip.lua",
-    dependencies = {
-        { "kkharji/sqlite.lua",           module = "sqlite" },
-        { "nvim-telescope/telescope.nvim" },
-    },
-    config = function()
-        require("neoclip").setup()
-    end,
-})
+return {
+	"AckslD/nvim-neoclip.lua",
+	dependencies = {
+		{ "kkharji/sqlite.lua", module = "sqlite" },
+		{ "nvim-telescope/telescope.nvim" },
+	},
+	config = function()
+		require("neoclip").setup()
+	end,
+}
