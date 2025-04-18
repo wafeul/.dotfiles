250fb54121356043261b9b415a2e69a16cc799ba
Raphaël Bourgeas <rbourgeas@embl.fr>
2025-04-18 16:39:33 +0200

    Removed comma

--- nvim/nvim/lua/plugins/neoclip.lua
+++ nvim/nvim/lua/plugins/neoclip.lua
@@ -2,7 +2,7 @@ require({
     "AckslD/nvim-neoclip.lua",
     dependencies = {
         { "kkharji/sqlite.lua",           module = "sqlite" },
-        { "nvim-telescope/telescope.nvim" },
+        { "nvim-telescope/telescope.nvim" }
     },
     config = function()
         require("neoclip").setup()
