697887025d1f5bf1dc0991bb4c91ef079983fc99
Raphaël Bourgeas <rbourgeas@embl.fr>
2025-04-18 16:38:29 +0200

    Add neoclip

---
+++ nvim/nvim/lua/plugins/neoclip.lua
@@ -0,0 +1,10 @@
+require({
+    "AckslD/nvim-neoclip.lua",
+    dependencies = {
+        { "kkharji/sqlite.lua",           module = "sqlite" },
+        { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+        require("neoclip").setup()
+    end,
+})
