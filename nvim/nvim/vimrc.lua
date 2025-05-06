32883f79eed4a0bc68cb466687915779197eb448
Raphaël Bourgeas <rbourgeas@embl.fr>
2025-05-06 09:41:32 +0200

    Close NvimTree when quitting NeoVim

--- nvim/nvim/vimrc.lua
+++ nvim/nvim/vimrc.lua
@@ -21,6 +21,31 @@ local function open_nvim_tree()
     -- open the tree.
     require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
 end
+
+-- Create an autocommand to close NvimTree when quitting Neovim
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(tree_wins, w)
+      end
+      if vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_wins, w)
+      end
+    end
+    if 1 == #wins - #floating_wins - #tree_wins then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end
+})
+
 -- Call the open_nvim_tree function after all plugins has been loaded ("VimEnter"). If not here, not working with alpha plugin.
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 require("keyboard-shortcuts")
