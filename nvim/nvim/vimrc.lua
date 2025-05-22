7e46a8e200ef1da409ec38bd21bb17a88d7d1fb9
Raphaël Bourgeas <rbourgeas@embl.fr>
2025-05-22 09:15:52 +0200

    removed phpcsfixer.php and added phpcs.xml

--- nvim/nvim/vimrc.lua
+++ nvim/nvim/vimrc.lua
@@ -4,58 +4,58 @@
 vim.opt.termguicolors = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
 local function open_nvim_tree()
-    -- open the tree.
-    require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+	-- open the tree.
+	require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
 end
 
 -- Close nvim-tree if it's the only window left in the tab
 vim.api.nvim_create_autocmd("QuitPre", {
-    callback = function()
-        local tree_wins = {}
-        local floating_wins = {}
-        local wins = vim.api.nvim_list_wins()
-        for _, w in ipairs(wins) do
-            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-            if bufname:match("NvimTree_") ~= nil then
-                table.insert(tree_wins, w)
-            end
-            if vim.api.nvim_win_get_config(w).relative ~= "" then
-                table.insert(floating_wins, w)
-            end
-        end
-        if 1 == #wins - #floating_wins - #tree_wins then
-            -- Should quit, so we close all invalid windows.
-            for _, w in ipairs(tree_wins) do
-                vim.api.nvim_win_close(w, true)
-            end
-        end
-    end,
+	callback = function()
+		local tree_wins = {}
+		local floating_wins = {}
+		local wins = vim.api.nvim_list_wins()
+		for _, w in ipairs(wins) do
+			local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+			if bufname:match("NvimTree_") ~= nil then
+				table.insert(tree_wins, w)
+			end
+			if vim.api.nvim_win_get_config(w).relative ~= "" then
+				table.insert(floating_wins, w)
+			end
+		end
+		if 1 == #wins - #floating_wins - #tree_wins then
+			-- Should quit, so we close all invalid windows.
+			for _, w in ipairs(tree_wins) do
+				vim.api.nvim_win_close(w, true)
+			end
+		end
+	end,
 })
 
 -- Disable hlsearch when moving the cursor
 vim.api.nvim_create_autocmd("CursorMoved", {
-    group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
-    callback = function()
-        if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
-            vim.schedule(function()
-                vim.cmd.nohlsearch()
-            end)
-        end
-    end,
+	group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+	callback = function()
+		if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+			vim.schedule(function()
+				vim.cmd.nohlsearch()
+			end)
+		end
+	end,
 })
 -- Call the open_nvim_tree function after all plugins has been loaded ("VimEnter"). If not here, not working with alpha plugin.
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
