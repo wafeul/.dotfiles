7ed8d88fb99812a96beaf8f54fda5be47870d7c0
Raphaël Bourgeas <rbourgeas@embl.fr>
2026-09-11 22:44:10 +0200

    fix +q4D73 issue

--- nvim/nvim/vimrc.lua
+++ nvim/nvim/vimrc.lua
@@ -2,6 +2,9 @@
 -- Source Lua Packer plugin
 -- require("plugins")
 vim.opt.termguicolors = true
+local termfeatures = vim.g.termfeatures or {}
+termfeatures.osc52 = false
+vim.g.termfeatures = termfeatures
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
